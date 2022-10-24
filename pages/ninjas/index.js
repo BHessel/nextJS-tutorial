791f892d0f6e3dd75caf54bdf8425b524961a9ed
Ben Hessel <hesselfitness@gmail.com>
2022-10-24 11:32:40 -0400

    all users listed, fetch made, getstaticprops function

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,24 @@
-const Ninjas = () => {
+import styles from "../../styles/User.module.css";
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return {
+    props: { users: users },
+  };
+};
+
+const Ninjas = ({ users }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
-      <h3>/ninjas</h3>
+      {users.map((user) => (
+        <div key={user.id}>
+          <a className={styles.single}>
+            <h3>{user.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
