4cad28d0b507be7a5208006238ab4d8daa5aa411
Ben Hessel <hesselfitness@gmail.com>
2022-10-24 10:46:15 -0400

    tutorial done through styles 1

---
+++ pages/ninjas/index.js
@@ -0,0 +1,10 @@
+const Ninjas = () => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      <h3>/ninjas</h3>
+    </div>
+  );
+};
+
+export default Ninjas;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <h3>/ninjas/test</h3>
+    </div>
+  );
+};
+
+export default Test;
